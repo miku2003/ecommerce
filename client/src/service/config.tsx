147919a91b1657fccb8ddb9d71dc529b92ff2dba
miku2003 <137183245+miku2003@users.noreply.github.com>
2025-06-13 14:50:06 +0530

    remove api

--- client/src/service/config.tsx
+++ client/src/service/config.tsx
@@ -2,7 +2,7 @@ import { Platform } from "react-native"
 
 export const BASE_URL = Platform.OS==='android' ? 'http://10.0.2.2:3000/api': 'http://localhost:3000/api'
 export const SOCKET_URL =  Platform.OS==='android' ? 'http://10.0.2.2:3000': 'http://localhost:3000'
-export const GOOGLE_MAP_API = "AIzaSyC51_aHWWvslwBvLAthKweFPY6bnsCc2N8"
+REACT_APP_GOOGLE_MAPS_API_KEY=your-new-google-api-key-here
 export const BRANCH_ID ='682a0a6a7d0cb18236b80411'
 
 // USE YOUR NETWORK IP OR HOSTED URL
